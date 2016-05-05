1346a9334b744e390ea0c324937658ef521e5379
Newton Jain <newton.jain@gmail.com>
2016-05-05 01:31:45 -0700

    Ionic side menu is configured

--- ionic-typescript-sidemenu/www/js/controllers.js
+++ ionic-typescript-sidemenu/www/js/controllers.js
@@ -1,66 +1,46 @@
-/// <reference path="../typings/tsd.d.ts" />
-var AppCtrl = (function () {
-    function AppCtrl($scope, $ionicModal, $timeout) {
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
-        var _this = this;
-        this.$scope = $scope;
-        this.$ionicModal = $ionicModal;
-        this.$timeout = $timeout;
-        // Form data for the login modal
-        this.loginData = {};
-        // Create the login modal that we will use later
-        $ionicModal.fromTemplateUrl("login.html", {
-            scope: this.$scope
-        }).then(function (modal) {
-            _this.modal = modal;
-        });
-    }
+angular.module('starter.controllers', [])
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    // Form data for the login modal
+    $scope.loginData = {};
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
     // Triggered in the login modal to close it
-    AppCtrl.prototype.closeLogin = function () {
-        this.modal.hide();
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
     };
     // Open the login modal
-    AppCtrl.prototype.login = function () {
-        this.modal.show();
+    $scope.login = function () {
+        $scope.modal.show();
     };
     // Perform the login action when the user submits the login form
-    AppCtrl.prototype.doLogin = function () {
-        var _this = this;
-        console.log("Doing login", this.loginData);
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
-        this.$timeout(function () {
-            _this.closeLogin();
+        $timeout(function () {
+            $scope.closeLogin();
         }, 1000);
     };
-    return AppCtrl;
-})();
-var PlaylistCtrl = (function () {
-    function PlaylistCtrl($stateParams) {
-        this.$stateParams = $stateParams;
-        this.playlistId = $stateParams["playlistId"];
-    }
-    return PlaylistCtrl;
-})();
-var PlaylistsCtrl = (function () {
-    function PlaylistsCtrl() {
-        this.playlists = [
-            { title: "Reggae", id: 1 },
-            { title: "Chill", id: 2 },
-            { title: "Dubstep", id: 3 },
-            { title: "Indie", id: 4 },
-            { title: "Rap", id: 5 },
-            { title: "Cowbell", id: 6 }
-        ];
-    }
-    return PlaylistsCtrl;
-})();
-angular.module('starter.controllers', [])
-    .controller('AppCtrl', AppCtrl)
-    .controller('PlaylistCtrl', PlaylistCtrl)
-    .controller('PlaylistsCtrl', PlaylistsCtrl);
+})
+    .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+        { title: 'ReggRido', id: 1 },
+        { title: 'Chill', id: 2 },
+        { title: 'Dubstep', id: 3 },
+        { title: 'Indie', id: 4 },
+        { title: 'Rap', id: 5 },
+        { title: 'Cowbell', id: 6 }
+    ];
+})
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+});
